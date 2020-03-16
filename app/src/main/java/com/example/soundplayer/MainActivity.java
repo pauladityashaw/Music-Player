465c95e4a0e40285ec045017708b3b61e2d91122
pauladityashaw <pauladityashaw@gmail.com>
2020-03-16 21:18:31 +0530

    1st commit

---
+++ app/src/main/java/com/example/soundplayer/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.soundplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer player;
+    SeekBar volumeSeekbar;
+    AudioManager audioManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        player = MediaPlayer.create(this, R.raw.song);
+        initControls();
+
+    }
+    public void play(View view) {
+        player.start();
+    }
+    public void pause(View view) {
+        player.pause();
+    }
+    private void initControls()
+    {
+        try
+        {
+            volumeSeekbar = findViewById(R.id.volume);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            volumeSeekbar.setMax(audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            volumeSeekbar.setProgress(audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC));
+
+
+            volumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+            {
+                @Override
+                public void onStopTrackingTouch(SeekBar arg0)
+                {
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar arg0)
+                {
+                }
+
+                @Override
+                public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
+                {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                            progress, 0);
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
